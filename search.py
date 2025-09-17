6487ead52db210d5d44feb9698327b51d1f1033a
Ami Raj <ami.raj2307@gmail.com>
2025-09-17 18:27:55 -0400

    Update search.py

--- search.py
+++ search.py
@@ -1,25 +1,21 @@
 def dfs(graph):
     visited = set()
-    nodes = sorted(graph.keys())
+    nodes = sorted(graph.keys())  
 
     for start_node in nodes:
         if start_node not in visited:
             stack = [start_node]
-
+            
             while stack:
                 node = stack.pop()
-
                 if node in visited:
                     continue
-
                 print(node)
                 visited.add(node)
-
                 if node in graph:
                     for nbr in sorted(graph[node], reverse=True):
                         if nbr not in visited:
                             stack.append(nbr)
-
 
 
 def bfs(graph):
