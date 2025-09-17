3af568de3889cea061a3c931f58f2881002f29f3
Ami Raj <ami.raj2307@gmail.com>
2025-09-16 21:41:20 -0400

    Create search.py

---
+++ search.py
@@ -0,0 +1,43 @@
+def dfs(graph):
+    visited = set()
+    nodes = sorted(graph.keys())
+    stack = []
+
+    for node in nodes:
+        if node not in visited:
+            stack.append(node)
+            while stack:
+                n = stack.pop()
+                if n in visited:
+                    continue
+                print(n)
+                visited.add(n)
+                if n in graph:
+                    for nbr in reversed(graph[n]):
+                        if nbr not in visited:
+                            stack.append(nbr)
+
+def bfs(graph):
+    visited = set()
+    nodes = sorted(graph.keys())
+    queue = []
+
+    for node in nodes:
+        if node not in visited:
+            queue.append(node)
+            while queue:
+                n = queue.pop(0)
+                if n in visited:
+                    continue
+                print(n)
+                visited.add(n)
+                if n in graph:
+                    for nbr in graph[n]:
+                        if nbr not in visited:
+                            queue.append(nbr)
+
+g = {1: [2, 3], 2: [], 3: [2, 4], 4: [3]}
+print("DFS order:")
+dfs(g)
+print("BFS order:")
+bfs(g)
