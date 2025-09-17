543a17eed811878a526bfd9e58602071fbbe8f3d
Ami Raj <ami.raj2307@gmail.com>
2025-09-16 21:54:49 -0400

    Update longest.py

--- longest.py
+++ longest.py
@@ -1,16 +1,9 @@
 def longest_lines(filename):
-    try:
-        with open(filename) as f:
-            lines = [line.rstrip('\n') for line in f]
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-        return
+    with open(filename) as f:
+        lines = [line.rstrip('\n') for line in f]
+    
     if not lines:
-        print([])  
-        return
+        return []
+
     max_length = max(map(len, lines))
-    result = [line for line in lines if len(line) == max_length]
-    print(result)
-
-longest_lines('foo.txt')
-
+    return [line for line in lines if len(line) == max_length]
